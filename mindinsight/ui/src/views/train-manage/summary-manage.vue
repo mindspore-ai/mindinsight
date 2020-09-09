4678bae53686e6dc51e73046374740e833d2696f
xiayifan <xiayifan1@huawei.com>
2020-09-09 16:51:59 +0800

    UI theme color update 1.graph theme 2.data-map theme color 3.tensor  theme color 4.scalar theme color

--- mindinsight/ui/src/views/train-manage/summary-manage.vue
+++ mindinsight/ui/src/views/train-manage/summary-manage.vue
@@ -16,7 +16,7 @@ limitations under the License.
 
 <template>
   <div id="cl-summary-manage">
-    <div v-if="loading"
+    <div v-show="loading"
          class="no-data-page">
       <div class="no-data-img">
         <img :src="require('@/assets/images/nodata.png')"
@@ -25,7 +25,7 @@ limitations under the License.
       </div>
     </div>
     <div class="cl-summary-manage-container"
-         v-if="!loading">
+         v-show="!loading">
       <div class="cl-title">
         <div class="cl-title-left">
           <span class="summary-title">{{$t('summaryManage.summaryList')}}</span>
@@ -33,6 +33,14 @@ limitations under the License.
           <span>{{$t('summaryManage.currentFolder')}}</span>
           <span :title="currentFolder">{{currentFolder}}</span>
           <span>{{$t("symbols.rightbracket")}}</span>
+          <div class="btn-wrap">
+            <el-button size="mini"
+                       class="custom-btn green"
+                       @click="goToTracebackAnalysis()">{{ $t('summaryManage.tracebackAnalysis') }}</el-button>
+            <el-button size="mini"
+                       class="custom-btn white"
+                       @click="goToCompareAnalysis()">{{ $t('summaryManage.compareAnalysis') }}</el-button>
+          </div>
         </div>
       </div>
 
@@ -56,11 +64,6 @@ limitations under the License.
                              show-overflow-tooltip>
             </el-table-column>
             <el-table-column width="180"
-                             prop="create_time"
-                             :label="$t('summaryManage.createTime')"
-                             show-overflow-tooltip>
-            </el-table-column>
-            <el-table-column width="180"
                              prop="update_time"
                              :label="$t('summaryManage.updateTime')"
                              show-overflow-tooltip>
@@ -68,22 +71,27 @@ limitations under the License.
             <!--operate   -->
             <el-table-column prop="operate"
                              :label="$t('summaryManage.operation')"
-                             width="240">
+                             class-name="operate-container"
+                             width="400">
               <template slot-scope="scope">
                 <span class="menu-item"
                       @contextmenu.prevent="rightClick(scope.row, $event, 0)"
                       @click.stop="goToTrainDashboard(scope.row)">
                   {{$t('summaryManage.viewDashboard')}} </span>
                 <span class="menu-item operate-btn"
-                           v-if="scope.row.viewProfiler"
-                           @contextmenu.prevent="rightClick(scope.row, $event, 1)"
-                           @click.stop="goToProfiler(scope.row)">
+                      v-if="scope.row.viewProfiler"
+                      @contextmenu.prevent="rightClick(scope.row, $event, 1)"
+                      @click.stop="goToProfiler(scope.row)">
                   {{$t('summaryManage.viewProfiler')}} </span>
-                <el-button type="text"
-                           class="operate-btn"
-                           disabled
-                           :title="$t('summaryManage.disableProfilerTip')"
-                           v-if="!scope.row.viewProfiler">{{$t('summaryManage.viewProfiler')}} </el-button>
+                <span class="menu-item operate-btn button-disable"
+                      v-else
+                      :title="$t('summaryManage.disableProfilerTip')">
+                  {{$t('summaryManage.viewProfiler')}}
+                </span>
+                <span class="menu-item operate-btn"
+                      @click.stop="showModelDialog(scope.row)">
+                  {{$t('summaryManage.paramDetails')}} </span>
+
               </template>
             </el-table-column>
           </el-table>
@@ -99,6 +107,32 @@ limitations under the License.
                        :total="pagination.total"
                        class="page">
         </el-pagination>
+      </div>
+      <!-- dialog of model details -->
+      <div v-if="showDialogModel">
+        <el-dialog :title="rowName"
+                   :visible.sync="showDialogModel"
+                   width="50%"
+                   :close-on-click-modal="false"
+                   class="details-data-list">
+          <el-table :data="modelData"
+                    row-key="id"
+                    lazy
+                    tooltip-effect="light"
+                    :load="loadDataListChildren"
+                    :tree-props="{children:'children',hasChildren:'hasChildren'}">
+            <el-table-column width="50" />
+            <el-table-column property="key"
+                             label="Key"
+                             width="200"></el-table-column>
+            <el-table-column property="value"
+                             label="Value">
+              <template slot-scope="scope">
+                {{scope.row.value}}
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-dialog>
       </div>
     </div>
     <div id="contextMenu"
@@ -119,7 +153,45 @@ export default {
     return {
       loading: true,
       currentFolder: '--',
+      // table filter condition
+      tableFilter: {lineage_type: {in: ['model']}},
+      showDialogModel: false,
       summaryList: [],
+      modelData: [],
+      objectType: 'object',
+      rowName: '--',
+      dialogKeys: {
+        train_dataset_path: {
+          label: this.$t('modelTraceback.trainSetPath'),
+        },
+        test_dataset_path: {
+          label: this.$t('modelTraceback.testSetPath'),
+        },
+        network: {
+          label: this.$t('modelTraceback.network'),
+        },
+        optimizer: {
+          label: this.$t('modelTraceback.optimizer'),
+        },
+        train_dataset_count: {
+          label: this.$t('modelTraceback.trainingSampleNum'),
+        },
+        test_dataset_count: {
+          label: this.$t('modelTraceback.testSampleNum'),
+        },
+        learning_rate: {
+          label: this.$t('modelTraceback.learningRate'),
+        },
+        device_num: {
+          label: this.$t('modelTraceback.deviceNum'),
+        },
+        model_size: {
+          label: this.$t('modelTraceback.modelSize'),
+        },
+        loss_function: {
+          label: this.$t('modelTraceback.lossFunc'),
+        },
+      },
       pagination: {
         currentPage: 1,
         pageSize: 16,
@@ -157,7 +229,9 @@ export default {
       window.addEventListener('resize', this.closeMenu, false);
       window.addEventListener('mousewheel', this.closeMenu, false);
       this.tableDom = this.$refs.table.bodyWrapper;
-      this.tableDom.addEventListener('scroll', this.closeMenu, false);
+      if (this.tableDom) {
+        this.tableDom.addEventListener('scroll', this.closeMenu, false);
+      }
     }, 300);
   },
 
@@ -191,7 +265,6 @@ export default {
                   );
                   summaryList.forEach((i) => {
                     i.relative_path = i.relative_path ? i.relative_path : '--';
-                    i.create_time = i.create_time ? i.create_time : '--';
                     i.update_time = i.update_time ? i.update_time : '--';
                     i.viewProfiler = i.profiler_dir && i.profiler_dir.length;
                   });
@@ -300,6 +373,101 @@ export default {
     closeMenu() {
       this.contextMenu.show = false;
     },
+    /**
+     * go to traceback analysis
+     */
+    goToTracebackAnalysis() {
+      this.$router.push({
+        path: '/model-traceback',
+      });
+    },
+    /**
+     * go to compare analysis
+     */
+    goToCompareAnalysis() {
+      this.$router.push({
+        path: '/compare-plate',
+      });
+    },
+    /**
+     * tree data
+     * @param {Object} tree
+     * @param {Object} treeNode
+     * @param {Object} resolve
+     */
+    loadDataListChildren(tree, treeNode, resolve) {
+      setTimeout(() => {
+        resolve(tree.children);
+      });
+    },
+    /**
+     * Show dialog of model
+     * @param {Object} row select row
+     */
+    showModelDialog(row) {
+      this.rowName =
+        row.train_id + ' ' + this.$t('summaryManage.trainingParamDetails');
+      const params = {
+        body: {},
+      };
+      this.tableFilter.summary_dir = {in: [row.train_id]};
+      params.body = Object.assign({}, this.tableFilter);
+      RequestService.queryLineagesData(params)
+          .then((resp) => {
+            this.showDialogModel = true;
+            if (
+              resp &&
+            resp.data &&
+            resp.data.object &&
+            resp.data.object.length
+            ) {
+              const resultArr = [];
+              const tempdata = resp.data.object[0].model_lineage;
+              const keys = Object.keys(tempdata);
+              keys.forEach((key, index) => {
+                const data = {
+                  id: index + 1,
+                  hasChildren: false,
+                  key: this.dialogKeys[key] ? this.dialogKeys[key].label : key,
+                  value: '',
+                };
+                if (tempdata[key] === null) {
+                  data.value = 'None';
+                } else if (
+                  typeof tempdata[key] === this.objectType &&
+                tempdata[key] !== null
+                ) {
+                  if (
+                    !(tempdata[key] instanceof Array) &&
+                  JSON.stringify(tempdata[key]) !== '{}'
+                  ) {
+                    data.hasChildren = true;
+                    data.children = [];
+                    Object.keys(tempdata[key]).forEach((k, j) => {
+                      const item = {};
+                      item.key = k;
+                      item.value = tempdata[key][k];
+                      item.id =
+                      `model` +
+                      `${new Date().getTime()}` +
+                      `${this.$store.state.tableId}`;
+                      this.$store.commit('increaseTableId');
+                      data.children.push(item);
+                    });
+                  }
+                  data.value = JSON.stringify(tempdata[key]);
+                } else {
+                  data.value = tempdata[key];
+                }
+                resultArr.push(data);
+              });
+              this.modelData = resultArr;
+            } else {
+              this.modelData = [];
+            }
+          })
+          .catch(() => {});
+    },
   },
   components: {},
 };
@@ -343,6 +511,9 @@ export default {
     padding-left: 0;
     height: 55px;
     line-height: 55px;
+    .btn-wrap {
+      float: right;
+    }
   }
   .summary-title {
     font-size: 20px;
@@ -357,6 +528,13 @@ export default {
     overflow-y: auto;
     .list-table {
       height: 100%;
+      .operate-container {
+        text-align: right;
+        padding-right: 32px;
+      }
+      th.operate-container {
+        text-align: center;
+      }
     }
   }
   .pagination-content {
@@ -365,6 +543,20 @@ export default {
   }
   .operate-btn {
     margin-left: 20px;
+    padding: 12px 0;
+  }
+  .el-dialog {
+    min-width: 500px;
+    padding-bottom: 30px;
+  }
+  .operate-btn.button-disable {
+    -moz-user-select: none; /*Firefox*/
+    -webkit-user-select: none; /*webkitbrowser*/
+    -ms-user-select: none; /*IE10*/
+    -khtml-user-select: none; /*Early browser*/
+    user-select: none;
+    color: rgb(192, 196, 204);
+    cursor: not-allowed;
   }
   .menu-item {
     color: #00a5a7;
@@ -386,6 +578,101 @@ export default {
         }
       }
     }
+  }
+  .details-data-list {
+    .el-table td,
+    .el-table th.is-leaf {
+      border: none;
+      border-top: 1px solid #ebeef5;
+    }
+    .el-table {
+      th {
+        padding: 10px 0;
+        border-top: 1px solid #ebeef5;
+        .cell {
+          border-left: 1px solid #d9d8dd;
+          height: 14px;
+          line-height: 14px;
+        }
+      }
+      th:first-child {
+        .cell {
+          border-left: none;
+        }
+      }
+      th:nth-child(2),
+      td:nth-child(2) {
+        max-width: 30%;
+      }
+      td {
+        padding: 8px 0;
+      }
+    }
+    .el-table__row--level-0 td:first-child:after {
+      width: 20px;
+      height: 1px;
+      background: #ebeef5;
+      z-index: 11;
+      position: absolute;
+      left: 0;
+      bottom: -1px;
+      content: '';
+      display: block;
+    }
+    .el-table__row--level-1 {
+      td {
+        padding: 4px 0;
+        position: relative;
+      }
+      td:first-child::before {
+        width: 42px;
+        background: #f0fdfd;
+        border-right: 2px #00a5a7 solid;
+        z-index: 10;
+        position: absolute;
+        left: 0;
+        top: -1px;
+        bottom: 0px;
+        content: '';
+        display: block;
+      }
+    }
+
+    .el-table__row--level-1:first-child {
+      td:first-child::before {
+        bottom: 0;
+      }
+    }
+    .el-dialog__title {
+      font-weight: bold;
+    }
+    .el-dialog__body {
+      max-height: 500px;
+      padding-top: 10px;
+      padding-bottom: 0px;
+      overflow: auto;
+      .details-data-title {
+        margin-bottom: 20px;
+      }
+    }
+  }
+  .custom-btn {
+    border: 1px solid #00a5a7;
+    border-radius: 2px;
+  }
+  .green {
+    background-color: #00a5a7;
+    color: white;
+  }
+  .white {
+    background-color: white;
+    color: #00a5a7;
+  }
+  .green:hover {
+    background-color: #33b7b9;
+  }
+  .white:hover {
+    background-color: #e9f7f7;
   }
 }
 </style>
