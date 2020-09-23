8e4f5509596a82e9f57f85463fc628eeba3fce4a
wangshuide2020 <7511764+wangshuide2020@user.noreply.gitee.com>
2020-09-23 15:18:01 +0800

    fix the problem of the duplication file.

--- mindinsight/mindconverter/graph_based_converter/mapper/impl/ops/__init__.py
+++ mindinsight/mindconverter/graph_based_converter/mapper/impl/ops/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implemented mapper."""
+"""Mapper implementation for ops."""
