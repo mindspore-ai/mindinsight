51d779585c0474e54e9cbec2203baefeec896764
luopengting <luopengting@huawei.com>
2020-09-25 10:11:22 +0800

    add annotations for __init__.py

--- mindinsight/lineagemgr/__init__.py
+++ mindinsight/lineagemgr/__init__.py
@@ -12,3 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""
+Lineage manager.
+
+Lineagemgr provides model training information, including data processing information,
+hyperparameters and model evaluation results, etc.
+"""
--- mindinsight/optimizer/common/__init__.py
+++ mindinsight/optimizer/common/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Test cases for querier."""
+"""Common modules for optimizer."""
