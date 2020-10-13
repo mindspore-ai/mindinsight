6fa5f8c5a80cb37014f02f7ea0371f8f7a380c65
caster <u6281911@anu.edu.au>
2020-10-13 11:53:23 +0800

    add parse_summary function
    we can use the function to transfer summary file to csv file and image so that it is easy to read and process

---
+++ mindinsight/datavisual/data_transform/summary_parser/image_writer.py
@@ -0,0 +1,56 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Image Writer.
+
+This module write scalar into a  csv file.
+"""
+import os
+import re
+
+from mindinsight.datavisual.data_transform.summary_parser.writer import Writer
+
+
+class ImageWriter(Writer):
+    """ImageWriter write image into a png file."""
+    def __init__(self, file_path):
+        """
+        Init ImageWriter.
+
+        Args:
+            file_path (str): A directory path, e.g. '/output/image/'.
+        """
+        self._file_path = file_path
+        self._image_data = []
+
+    def add(self, value):
+        """
+        Add value.
+
+        Args:
+            value (object): tag and step and image value.
+        """
+        self._image_data.append(value)
+
+    def write(self):
+        """Write file."""
+        for i in range(len(self._image_data)):
+            tag = self._image_data[i][0]
+            tag = tag.replace('/', '_')
+            tag = re.sub(r'[^a-zA-Z0-9_]+', '', tag)
+            with os.fdopen(os.open("{}/{}_{}.png".format(self._file_path, tag, self._image_data[i][1]),
+                                   os.O_WRONLY | os.O_CREAT, 0o600), 'wb') as fp:
+                fp.write(self._image_data[i][2])
+        self._image_data = []
---
+++ mindinsight/datavisual/data_transform/summary_parser/scalar_writer.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Scalar Writer.
+
+This module write scalar into a csv file.
+"""
+import csv
+import os
+
+from mindinsight.datavisual.data_transform.summary_parser.writer import Writer
+
+
+class ScalarWriter(Writer):
+    """ScalarWriter write scalar into a  csv file."""
+    def __init__(self, file_path):
+        """
+        Init ScalarWriter.
+
+        Args:
+            file_path (str): A directory path, e.g. '/output/'.
+        """
+        self._file_path = file_path
+        self._scalar_data = [("tag", "step", "wall_time", "value")]
+
+    def add(self, value):
+        """
+        Add value.
+
+        Args:
+            value (object): wall_time, tag and step and scalar value.
+        """
+        self._scalar_data.append(value)
+
+    def write(self):
+        """Write file."""
+        with os.fdopen(os.open('{}/scalar.csv'.format(self._file_path), os.O_WRONLY | os.O_CREAT, 0o600), 'w') as fp:
+            writer = csv.writer(fp, dialect='excel')
+            writer.writerows(self._scalar_data)
