63feb589d8905a02649aa4025d6d70a386d245d7
weiyanxi <weiyanxi@huawei.com>
2021-03-15 18:54:50 +0800

    UI / Memory detail page add button to show table full screen, cpu detail adjust left padding of charts

--- mindinsight/ui/src/views/profiling-gpu/resource-utilization.vue
+++ mindinsight/ui/src/views/profiling-gpu/resource-utilization.vue
@@ -124,7 +124,7 @@ export default {
             },
           ],
           grid: {
-            left: 40,
+            left: 60,
             top: 40,
             right: 70,
             bottom: 60,
