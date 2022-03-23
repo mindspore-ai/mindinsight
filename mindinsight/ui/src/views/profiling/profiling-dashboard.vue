c4637a9659cb88573a262d910dc3f69e5bef6c94
mahequn123 <mahequn1@huawei.com>
2022-03-23 20:15:48 +0800

    Adjust label spacing in pynative mode

--- mindinsight/ui/src/views/profiling/profiling-dashboard.vue
+++ mindinsight/ui/src/views/profiling/profiling-dashboard.vue
@@ -66,11 +66,17 @@ export default {
   updated() {
     this.updateTab();
   },
-  mounted() {
-    let length = 0;
-    this.tabs.forEach((t) => length += t.length);
-    // 10(px of en-us char), 20(px of zh-cn char), 40(padding) used to calculate the position of path label
-    this.left = length * (this.$store.state.language === 'en-us' ? 10 : 20) + 40;
+  watch: {
+    isPynative: {
+      handler() {
+        let length = 0;
+        this.tabs.forEach((t) => length += t.length);
+        // 10(px of en-us char), 20(px of zh-cn char), 40(padding) used to calculate the position of path label
+        this.left = this.isPynative? length * (this.$store.state.language === 'en-us' ? 10 : 20): length * (this.$store.state.language === 'en-us' ? 10 : 20) + 40;
+      },
+      immediate: true
+    }
+    
   },
   methods: {
     /**
