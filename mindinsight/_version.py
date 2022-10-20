7f5683df9b7d427846ffd58880463e79ab534d88
liuchuting <liuchuting1@huawei.com>
2022-10-20 09:56:24 +0800

    change version 1.9.0 to 2.0.0

--- mindinsight/_version.py
+++ mindinsight/_version.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Huawei Technologies Co., Ltd
+# Copyright 2020-2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # ============================================================================
 """Mindinsight version module."""
 
-VERSION = '1.9.0'
+VERSION = '2.0.0'
